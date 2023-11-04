74a5d942917afd39a26d37b863109e9697bfad70
OlivierMaria <olivier-maria@hotmail.com>
2023-11-04 21:34:24 +0100

    Added text for information in the contact part

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./contact.css";
 import MapView from "../../components/MapView/MapView.jsx";
+import aksil from "../../assets/aksil.jpg";
+
+import { BiTime, BiMapPin, BiPhoneCall } from "react-icons/bi";
 
 function Contact() {
   return (
@@ -8,7 +11,29 @@ function Contact() {
       <h4>C'est trés simple</h4>
       <h2>Nous retrouver</h2>
       <div className="aksil__contact-content">
-        <div className="aksil__contact-content_text"></div>
+        <div className="aksil__contact-content_text ">
+          <div className="aksil__contatc-content__p">
+            <BiTime />{" "}
+            <p>
+              Votre restaurant est ouver de 10 heures à 23 heures 7j/7 sans
+              intéruption.
+            </p>
+          </div>
+          <div className="aksil__contatc-content__p">
+            <BiMapPin />{" "}
+            <p>
+              Nous sommes situé au 130 Boulevard Gambetta 06000 Nice. Vous
+              pouvez aussi consultez la carte.
+            </p>
+          </div>
+          <div className="aksil__contatc-content__p">
+            <BiPhoneCall />{" "}
+            <p>
+              Apellez-nous au 09 83 47 60 33. Nous restons disponible pour
+              toutes commandes et/ou informations
+            </p>
+          </div>
+        </div>
         <div className="aksil__contact-content_map">
           <MapView />
         </div>
